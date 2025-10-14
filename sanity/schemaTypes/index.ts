0e56e1e7912de5bf4776284dc7fb0576b2d6e0cf
João Vitor Poliani <joaovitorpolianijvp@gmail.com>
2025-10-14 20:22:47 -0300

    Removido a pasta sanity

--- sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import {alunoType} from './alunoType'
-import {clienteType} from './clienteType'
-import {cursoType} from './cursoType'
-import {feedbacksType} from './feedbackType'
-import {resultadoType} from './resultadoType'
-
-
-export const schemaTypes = [alunoType, clienteType, cursoType, feedbacksType, resultadoType]
